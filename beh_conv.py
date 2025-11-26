a260683052bf426e6a49b253b26e1ca556fcc293
Emanuele Porcu <porcu.emanuele@gmail.com>
2025-11-26 16:08:59 +0100

    refactor code

--- beh_conv.py
+++ beh_conv.py
@@ -4,119 +4,97 @@ import warnings
 
 import metadata_templates as metadata
 
-import yaml
 import pandas as pd
 from scipy.io import loadmat
 
-class Beh2Bids:
+
+class BehToBids:
     """
+    Converts Behavioral data
+    to BIDS format, right now it accepts only
+    tabular data such as .txt, .csv
+    and .txt and .mat files as long as
+    they are not nested
+
     Parameters
     ----------
-    n_subjects : int, number of subjects
-    n_sessions : int, optional number of sessions
-    subjects : list | tuple (optional) of strings that specify
-               the subject label. If not specified, it creates
-               labels based on n_subjects (e.g. sub-01, sub-02 etc.)  
-    dataset : str, optional, dataset name
-    data_dir : Path | str output directory
-    sessions : list | tuple, optional, of strings that specify sessions
-               names, if not specified, it creates session names as
-               'ses-01'.
+    data : dict
+        bids_dir : str, bids data directory
+        data : dict, a dictionary containing as
+               keys the subject ID meant to create the
+               sub-<label> entity and list of data as values.
+               Sessions must be a dictionary of each subject ID
+               and must contain as keys the session labels and
+               as values a list of data per session key
+        col_names : dict | None (optional), keys are original column
+                    names and values are bids compliant
+                    column names (snake_case)
+        datatype : str, 'beh' or 'events'
+        task_label : str, task label for
+                    the task-<label> entity
+                    as defined by BIDS
+        acq_label : str | None (optional), acquisition label
+                   for acq-<label> entity as
+                   defined by BIDS
+        participants_columns : list (optional), list of
+                    custom columns
+        inherit : bool, True by default, if True json sidecars
+                  are inhereted otherwise
+                  each files is provided with its
+                  sidecar
+        neuro_suffix : str | None (optional) in case of events
+                       conversion, it indicates the
+                       suffix of the neuroimaging
+                       recording, mandatory when
+                       converting events
+
+    Example:
+
+    INFO: {
+            "bids_dir": "rawdata",
+            "data": {
+                    01: {
+                        'predrug': [data],
+                        'postdrug': [data]
+                        },
+                    02: {
+                        'predrug': [data],
+                        'postdrug': [data]
+                        },
+                    ...
+                    },
+            "col_names": {
+                "orignal Name": "new_bids_compliant_name",
+                "Col Name": "col_name",
+                ...
+            },
+            "datatype": "beh",
+            "task_label": "task name",
+            "acq_label": None,
+            "participants_columns": None,
+            "inherit": True,
+            "neuro_suffix": None,
+            }
     """
-    def __init__(
-            self,
-            n_subjects,
-            n_sessions=None,
-            subjects=None, 
-            sessions=None,
-            dataset=None,
-            participants_columns=None, 
-            bids_dir="bids"
-            ):
-        self.n_subjects = n_subjects
-        self.n_sessions = n_sessions
-        self.dataset = dataset
-        self.subjects = subjects
-        self.sessions = sessions
-        self.participants_columns = participants_columns
-        self.bids_dir = bids_dir
 
-    def save_json(self, data, filename):
-        """
-        Save json file
-        """
-        with open(f'{filename}.json', 'w', encoding='utf-8') as f:
-            json.dump(data, f, indent=4)
-
-    def _create_sessions(self):
-
-        """
-        """
-        pass
-
-    def _data2df(self, data):
-        """ 
-        Reads data, either .csv or simple .mat files.
-        Parameters
-        ----------
-        data : .csv | .mat files, for now .mat 
-               cannot be complicated nested files
-               
-        Returns
-        -------
-        df : pandas DataFrame with columns derived by 
-             the original files
-        """
-        try:
-            print("Trying to open a .csv file...")
-            df = pd.read_csv(data)
-        except:
-            print('no .csv file found')
-            try:
-                print('Trying to get a .mat file...')
-                df = pd.DataFrame(loadmat(data))
-            except:
-                pass
-        return df
-
-    def _to_bids(self, df, col_names=None):
-        """
-        Rename original columns in bids compliant column names
-        TODO: check whether onsets and duration are in seconds
-        
-        Parameters
-        ----------
-        df : pandas DataFrame with data
-        col_names : None | dict, with original names as keys and
-                    bids compliant names as values
-        Returns
-        -------
-        df : pandas DataFrame, with correct column names
-        """
-
-        if col_names:
-            df = df.rename(columns=col_names)
-        print(df.columns)
-        df = df.drop(columns=["Unnamed: 0"])
-        return df
-
-    def _create_sidecar(self, df, filename):
-        """
-        Create sidecars for _beh/_events files
-        """
-        json_dict = {col: None for col in df.columns}
-        # Add template column text
-        self.save_json(json_dict, filename)
+    def __init__(self, data):
+        self.data = data
+        # Set optional keys to None or default values
+        self.data["col_names"] = None
+        self.data["acq_label"] = None
+        self.data["participants_columns"] = None
+        self.data["inherit"] = True
+        self.data["neuro_suffix"] = None
 
     def _check_labels(self, labels):
-        """ 
+        """
         Check whether labels are
         alphanumeric and are all different
         Parameters
         ----------
-        
+
         labels : list or tuple of strings
-        
+
         Returns
         -------
 
@@ -126,7 +104,7 @@ class Beh2Bids:
         for n, i in enumerate(labels):
             if not i.isalnum():
                 valid = False
-            for j in labels[n+1:]:
+            for j in labels[n + 1 :]:
                 if i.lower() == j.lower():
                     valid = False
         return valid
@@ -134,133 +112,187 @@ class Beh2Bids:
     def is_info_valid(self):
         """
         Check whether provided inputs
-        are valid
+        are BIDS compliant
         """
-        if self.subjects is not None:
-            assert self._check_labels(self.subjects), \
-                    "subject labels must be all different and alphanumeric"
-            assert len(self.subjects) == self.n_subjects, \
-                    "subject labels and number of subjects do not match"
+        sub_labels = list(self.data["data"].keys())
+        assert self._check_labels(
+            sub_labels
+        ), "subject labels must be all different and alphanumeric"
 
-        if self.sessions is not None:
-            assert self._check_labels(self.sessions), \
-                "sessions labels must be all different and alphanumeric"
+        if isinstance(list(self.data["data"].values())[0], dict):
+            ses_labels = {
+                label for ses in self.data["data"].values() for label in ses.keys()
+            }
+            assert self._check_labels(
+                list(ses_labels)
+            ), "sessions labels must be all different and alphanumeric"
 
     def create_bids_struct(self):
         """
         Create a bids boilerplate
         """
+        # check inputs validity
+        self.is_info_valid()
         # create root directory
-        if not Path(self.bids_dir).exists():
-            Path(self.bids_dir).mkdir()
-        # create metadata files
-        metadata.create_participants(
-                self.bids_dir,
-                self.n_subjects,
-                self.subjects, 
-                self.participants_columns
-                )
-        metadata.create_dataset_description(self.bids_dir)
-        metadata.root_metadata(self.bids_dir)
+        if not Path(self.data["bids_dir"]).exists():
+            Path(self.data["bids_dir"]).mkdir()
 
-        subjects_list = [
-                f"sub-{sub+1:0{len(str(self.n_subjects))}}"
-                for sub in range(self.n_subjects)
-                ]
-        if self.subjects is not None:
-            subjects_list = self.subjects
-
-        if self.sessions or self.n_sessions:
-            sessions_list = [
-                        f"ses-{ses+1:0{len(str(self.n_sessions))}}"
-                        for ses in range(self.n_sessions)
-                        ]
-            if self.sessions is not None:
-                sessions_list = self.sessions
-
-            for sub in subjects_list:
-                for ses in sessions_list:
-                    data_out = Path(self.bids_dir) / Path(sub) / Path(ses) / "beh"
+        subjects_list = []
+        for sub, val in self.data["data"].items():
+            subjects_list.append(f"sub-{sub}")
+            ## create sub dir
+            if isinstance(val, dict):
+                ## create ses dir and and beh dir
+                for ses in val.keys():
+                    data_out = (
+                        Path(self.data["bids_dir"])
+                        / Path(f"sub-{sub}")
+                        / Path(f"ses-{ses}")
+                        / "beh"
+                    )
                     data_out.mkdir(parents=True, exist_ok=True)
-        else:
-            for sub in subjects_list:
-                data_out = Path(self.bids_dir) / Path(sub) / "beh"
+            else:
+                # create beh dir
+                data_out = Path(self.data["bids_dir"]) / Path(f"sub-{sub}") / "beh"
                 data_out.mkdir(parents=True, exist_ok=True)
 
-    def __call__(self, data):
-        """
-        """
-        if data['datatype'] == "beh":
-            suffix = "beh"
-            # create a structure for bids
-            self.create_bids_struct()
-            # check columns
-        elif data['datatype'] == "events":
-            suffix = "events"
-            if "onset" not in data or "duration" not in data:
-                warnings.warn("You need the onset and duration columns for events, unless you have a resting state study")
-        else:
-            raise NameError(f"{data['datatype']} does not exist.\n You must select between 'beh' or 'events'")
+        # create metadata files
+        metadata.create_participants(
+            self.data["bids_dir"], subjects_list, self.data["participants_columns"]
+        )
+        metadata.create_dataset_description(self.data["bids_dir"])
+        metadata.root_metadata(self.data["bids_dir"])
 
-        df = self._to_bids(
-                self._data2df(data['datadir']),
-                data['col_names']
-                )
-        if data['outdir'] is not None:
-            basedir = f"{data['outdir']}"
-        elif data['outdir'] is None and data["datatype"] == "events":
-            raise ValueError("You should specify an output directory")
-        else:
-            basedir = Path(self.bids_dir) / Path(f"sub-{data['sub']}") / "beh"
-            if data["session"]:
+    def _data2df(self, data):
+        """
+        Reads data, either .csv or simple .mat files.
+        Parameters
+        ----------
+        data : .csv | .mat files, for now .mat
+               cannot be complicated nested files
+
+        Returns
+        -------
+        df : pandas DataFrame with columns derived by
+             the original files
+        """
+        try:
+            df = pd.read_csv(data, index_col=False, sep="\t")
+        except:
+            print("no .csv file found")
+            try:
+                df = pd.DataFrame(loadmat(data))
+            except:
+                pass
+        return df
+
+    def _to_bids(self, df, col_names=None):
+        """
+        Rename original columns in bids compliant column names,
+        make column names lower capital
+        TODO: check whether onsets and duration are in seconds
+
+        Parameters
+        ----------
+        df : pandas DataFrame with data
+        col_names : None | dict, with original names as keys and
+                    bids compliant names as values
+        Returns
+        -------
+        df : pandas DataFrame, with correct column names
+        """
+        if col_names:
+            df = df.rename(columns=col_names)
+        df = df.rename(columns={i: i.lower() for i in df.columns})
+        if "Unnamed: 0" in df.columns:
+            df = df.drop(["Unnamed: 0"], axis=1)
+        df = df.fillna("n/a")
+        return df
+
+    def _get_file_names(self, sub, session, run, task_label, acq_label, datatype):
+        """
+        Create all the necessary filename
+        and directory names
+        """
+        if self.data["datatype"] == "events":
+            basedir = (
+                Path(self.data["bids_dir"])
+                / Path(f"sub-{sub}")
+                / self.data["neuro_suffix"]
+            )
+            if session:
                 basedir = (
-                    Path(self.bids_dir) / Path(f"sub-{data['sub']}") / Path(f"ses-{data['session']}") / "beh"
+                    Path(self.data["bids_dir"])
+                    / Path(f"sub-{sub}")
+                    / Path(f"ses-{session}")
+                    / self.data["neuro_suffix"]
                 )
-
-        if data['acq_label']:
-            if data["session"]:
-                filename = "_".join(
-                    (
-                        f"sub-{data['sub']}",
-                        f"ses-{data['session']}",
-                        f"task-{data['task_label']}",
-                        f"acq-{data['acq_label']}",
-                        f"run-{data['run']}",
-                        f"{suffix}"
-                        )
-                    )
-            else: 
-                filename = "_".join(
-                        (
-                        f"sub-{data['sub']}",
-                        f"task-{data['task_label']}",
-                        f"acq-{data['acq_label']}",
-                        f"run-{data['run']}",
-                        f"{suffix}"
-                        )
-                    )
         else:
-            if data["session"]:
-                filename = "_".join(
-                        (
-                        f"sub-{data['sub']}",
-                        f"ses-{data['session']}",
-                        f"task-{data['task_label']}",
-                        f"run-{data['run']}",
-                        f"{suffix}"
-                        )
-                    )
-            else:
-                filename = "_".join(
-                    (
-                        f"sub-{data['sub']}",
-                        f"task-{data['task_label']}",
-                        f"run-{data['run']}",
-                        f"{suffix}"
-                        )
-                    )
+            basedir = Path(self.data["bids_dir"]) / Path(f"sub-{sub}") / "beh"
+            if session:
+                basedir = (
+                    Path(self.data["bids_dir"])
+                    / Path(f"sub-{sub}")
+                    / Path(f"ses-{session}")
+                    / "beh"
+                )
+        file_dict = {"ses": session, "task": task_label, "acq": acq_label, "run": run}
+        filename = f"sub-{sub}"
+        for key, val in file_dict.items():
+            if val is not None:
+                filename = f"{filename}_{key}-{val}"
+        filename = f"{filename}_{datatype}"
+        return filename, basedir
 
-        df.to_csv(
-            basedir / f"{filename}.tsv",
-            sep='\t')
-        self._create_sidecar(df, basedir / f"{filename}")
+    def convert(self):
+        """
+        The actual data conversion
+        """
+        for sub, val in self.data["data"].items():
+            if isinstance(val, dict):
+                for ses in val.keys():
+                    for run, data in enumerate(val[ses]):
+                        if len(val) == 1:
+                            run_index = None
+                        else:
+                            run_index = run + 1
+                        filename, basedir = self._get_file_names(
+                            sub,
+                            ses,
+                            run_index,
+                            self.data["task_label"],
+                            self.data["acq_label"],
+                            self.data["datatype"],
+                        )
+                        df = self._to_bids(self._data2df(data), self.data["col_names"])
+                        df.to_csv(basedir / f"{filename}.tsv", index=False)
+                        metadata._create_sidecar(
+                            df,
+                            self.data["inherit"],
+                            self.data["task_label"],
+                            basedir / f"{filename}",
+                            self.data["bids_dir"],
+                        )
+            else:
+                for run, data in enumerate(val):
+                    if len(val) == 1:
+                        run_index = None
+                    else:
+                        run_index = run + 1
+                    filename, basedir = self._get_file_names(
+                        sub,
+                        None,
+                        run_index,
+                        self.data["task_label"],
+                        self.data["acq_label"],
+                        self.data["datatype"],
+                    )
+                    df = self._to_bids(self._data2df(data), self.data["col_names"])
+                    df.to_csv(basedir / f"{filename}.tsv", index=False)
+                    metadata._create_sidecar(
+                        df,
+                        self.data["inherit"],
+                        self.data["task_label"],
+                        basedir / f"{filename}",
+                        self.data["bids_dir"],
+                    )
